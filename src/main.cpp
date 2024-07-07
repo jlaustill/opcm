9530746d73a5492147db7e7f3f1e37ffbe4413ca
AlxBilt <144752076+AlxBilt@users.noreply.github.com>
2024-07-06 22:00:02 -0400

    Simple Blink Option

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,36 @@
 
 #include "Domain/opcm.h"
 
+//Begin Led Blink
+
+// Teensy 2.0 has the LED on pin 11
+// Teensy++ 2.0 has the LED on pin 6
+// Teensy 3.x / Teensy LC have the LED on pin 13
+const int ledPin = 13;
+// End Led Blink
+
 void setup()
 {
     opcm::setup();
+     // Led Blink Setup
+  // initialize the digital pin as an output.
+  pinMode(ledPin, OUTPUT);
+  for(;;)
+//Led Blink Setup End
+
+{
+ //Led Loop Begins
+  digitalWrite(ledPin, HIGH);   // set the LED on
+  delay(1000);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // set the LED off
+  delay(1000);                  // wait for a second
+//Led Loop  Ends
+}
 }
 
 __attribute__((unused)) void loop()
+
 {
     opcm::loop();
 }
+
