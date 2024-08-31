87148ef55b64051a4d71a97ad8b96fc51e55784b
Joshua Lawrence Austill <jlaustill@gmail.com>
2024-08-31 09:18:20 -0600

    this isn't working blah

--- src/Data/CumminsBus.cpp
+++ src/Data/CumminsBus.cpp
@@ -525,17 +525,6 @@ int8_t CumminsBus::getSelectedGear() {
   return static_cast<int8_t>(pgn61445.data[0] - 125);
 }
 
-byte CumminsBus::getVehicleSpeed() {
-  // Compute Vehicle Speed
-  uint32_t speedRaw = (pgn61442.data[2] << 8) | pgn61442.data[1];
-  uint32_t outputShaftRpm = speedRaw * .125;
-  float wheelRpm = outputShaftRpm / 3.73;
-  float inchesPerMinute = wheelRpm * 100.11;
-  float milesPerHour = inchesPerMinute * 60 / 63360;
-
-  return static_cast<byte>(milesPerHour);
-}
-
 void CumminsBus::initialize() {
   Serial.println("Cummins Bus initializing");
 
